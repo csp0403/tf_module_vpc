84ee4b03cf32b54e69f2fdfe8d160cce11741617
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-12-26 13:18:16 +0530

    just11

--- main.tf
+++ main.tf
@@ -4,12 +4,13 @@ resource "aws_vpc" "main" {
   tags = merge(local.common_tags,{ Name = "${var.env}-vpc" })
 
 }
-resource "aws_subnet" "main_subnet" {
+resource "aws_subnet" "main" {
   count      = length(var.subnets_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.subnets_cidr[count.index]
 
-
-  tags = merge(local.common_tags,{ Name = "${var.env}-subnet${count.index+1}"} )
-
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-subnet-${count.index + 1}" }
+  )
 }
